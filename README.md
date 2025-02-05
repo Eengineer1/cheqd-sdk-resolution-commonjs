0d9e15b761c8960af93b4cbb1333632b1d2acc2b
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2025-02-05 16:17:11 +0200

    Added CJS evidently

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sample CommonJS restrictive module, loading `@cheqd/sdk` all-inclusive release. Use as a reference for your own project
+
+Run the following successive commands to install the dependencies and run the sample:
+
+```bash
+npm install
+
+npm run build
+
+node dist/index.js
+```
+
+> **Note:** Successful transactions will be printed in the console, with a `code` of `0` and a `transactionHash` of the transaction.
---
+++ src/index.ts
@@ -0,0 +1,85 @@
+import {
+  AbstractCheqdSDKModule,
+  CheqdNetwork,
+  CheqdSDK,
+  createCheqdSDK,
+  createDidPayload,
+  createDidVerificationMethod,
+  createKeyPairBase64,
+  createVerificationKeys,
+  DIDModule,
+  FeemarketModule,
+  ICheqdSDKOptions,
+  ISignInputs,
+  MethodSpecificIdAlgo,
+  VerificationMethods,
+} from "@cheqd/sdk";
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { fromString, toString } from "uint8arrays";
+
+const run = async () => {
+  // define options
+  const options = {
+    modules: [FeemarketModule as unknown as AbstractCheqdSDKModule, DIDModule as unknown as AbstractCheqdSDKModule],
+    rpcUrl: "https://rpc.cheqd.network:443",
+    network: CheqdNetwork.Testnet,
+    wallet: await DirectSecp256k1HdWallet.fromMnemonic(
+      "sketch mountain erode window enact net enrich smoke claim kangaroo another visual write meat latin bacon pulp similar forum guilt father state erase bright",
+    { prefix: "cheqd" }),
+  } satisfies ICheqdSDKOptions;
+
+  // create cheqd sdk
+  const cheqdSDK = await createCheqdSDK(options);
+
+  // create keypair
+  const keyPair = createKeyPairBase64();
+
+  // create verification keys
+  const verificationKeys = createVerificationKeys(
+    keyPair.publicKey,
+    MethodSpecificIdAlgo.Uuid,
+    "key-1"
+  );
+
+  // create verification methods
+  const verificationMethods = createDidVerificationMethod(
+    [VerificationMethods.Ed255192020],
+    [verificationKeys]
+  );
+
+  // create did document
+  const didDocument = createDidPayload(verificationMethods, [verificationKeys]);
+
+  // create sign inputs
+  const signInputs = [
+    {
+      verificationMethodId: didDocument.verificationMethod![0].id as string,
+      privateKeyHex: toString(fromString(keyPair.privateKey, "base64"), "hex"),
+    },
+  ] satisfies ISignInputs[];
+
+  // define fee payer
+  const feePayer = (await options.wallet.getAccounts())[0].address;
+
+  // define fee amount
+  const fee = await DIDModule.generateCreateDidDocFees(feePayer);
+
+  console.warn('cheqdSDK:', cheqdSDK);
+
+  // create did
+  const createDidDocResponse = await cheqdSDK.createDidDocTx(
+    signInputs,
+    didDocument,
+    feePayer,
+    fee,
+    undefined,
+    undefined,
+    { sdk: cheqdSDK }
+  )
+
+  console.warn('did document:', JSON.stringify(didDocument, null, 2));
+
+  console.warn('did tx:', JSON.stringify(createDidDocResponse, null, 2));
+};
+
+run().catch(console.error);
